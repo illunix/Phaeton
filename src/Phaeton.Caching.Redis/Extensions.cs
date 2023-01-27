5106fb9e1cbe15130f5c4a3d1b9812117a359545
Maks Szokalski <42069493+illunix@users.noreply.github.com>
2023-01-27 19:34:02 +0100

    update

---
+++ src/Phaeton.Caching.Redis/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.OutputCaching;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using StackExchange.Redis;
+
+namespace Phaeton.Caching.Redis;
+
+public static class Extensions
+{
+    public static IServiceCollection AddRedisOutputCache(
+        this IServiceCollection services,
+        IConfiguration config
+    )
+    {
+        var section = config.GetSection("redisCache");
+        var options = section.BindOptions<RedisCachingOptions>();
+        services.Configure<RedisCachingOptions>(section);
+
+        if (!options.Enabled)
+            return services;
+
+        if (string.IsNullOrEmpty(options.Endpoint))
+            ArgumentException.ThrowIfNullOrEmpty(options.Endpoint);
+
+        services
+            .AddSingleton<IConnectionMultiplexer>(q => ConnectionMultiplexer.Connect(new ConfigurationOptions()
+            {
+                EndPoints = { options.Endpoint! },
+                AbortOnConnectFail = false
+            }))
+            .AddOutputCache()
+            .RemoveAll<IOutputCacheStore>()
+            .AddSingleton<IOutputCacheStore, RedisOuputCacheStore>();
+
+        return services;
+    }
+}
---
+++ src/Phaeton.Caching.Redis/RedisOuputCacheStore.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.OutputCaching;
+using StackExchange.Redis;
+
+namespace Phaeton.Caching.Redis;
+
+public sealed class RedisOuputCacheStore : IOutputCacheStore
+{
+    private readonly IDatabaseAsync _db;
+
+    public RedisOuputCacheStore(IConnectionMultiplexer connMultiplexer)
+        => _db = connMultiplexer.GetDatabase();
+
+    public async ValueTask<byte[]?> GetAsync(
+        string key,
+        CancellationToken ct = default
+    )
+    {
+        ArgumentNullException.ThrowIfNull(key);
+
+        return await _db.StringGetAsync(key);
+    }
+
+    public async ValueTask SetAsync(
+        string key,
+        byte[] value,
+        string[]? tags,
+        TimeSpan validFor,
+        CancellationToken ct = default
+    )
+    {
+        ArgumentNullException.ThrowIfNull(key);
+        ArgumentNullException.ThrowIfNull(value);
+
+        foreach (var tag in tags ?? Array.Empty<string>())
+        {
+            await _db.SetAddAsync(
+                tag,
+                key
+            );
+        }
+
+        await _db.StringSetAsync(
+            key,
+            value,
+            validFor
+        );
+    }
+
+    public async ValueTask EvictByTagAsync(
+        string tag,
+        CancellationToken ct = default
+    )
+    {
+        ArgumentNullException.ThrowIfNull(tag);
+
+        var cachedKeys = await _db.SetMembersAsync(tag);
+        var keys = cachedKeys
+            .Select(q => (RedisKey)q.ToString())
+            .Concat(new[] { (RedisKey)tag })
+            .ToArray();
+
+        await _db.KeyDeleteAsync(keys);
+    }
+}
